13ad1aa8cc4c4aa24a04b52d7b4dac99d75639c2
Shahid Afridi <115124671+Shahidafridi3303@users.noreply.github.com>
2025-02-27 22:23:37 -0500

    Setted map, created physics material bouncy for our ball and ball script to adjust Rb runtime

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public CircleCollider2D col;
+
+    [HideInInspector] public Vector3 pos { get { return transform.position; } }
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<CircleCollider2D>();
+    }
+
+    public void Push(Vector2 force)
+    {
+        rb.AddForce(force, ForceMode2D.Impulse);
+    }
+
+    public void ActivateRb()
+    {
+        rb.isKinematic = false;
+    }
+
+    public void DeactivateRb()
+    {
+        rb.velocity = Vector2.zero;
+        rb.angularVelocity = 0f;
+        rb.isKinematic = true;
+    }
+}
